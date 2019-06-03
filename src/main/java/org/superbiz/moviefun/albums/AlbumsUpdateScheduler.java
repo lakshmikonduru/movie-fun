f6d395aac2af0fa5fdefed74cb2eb4376195b197
Lakshmi Konduru <CT0t1868@TechMahindra.com>
2019-06-03 14:18:17 +0530

    background jobs changes

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
@@ -19,35 +19,29 @@ public class AlbumsUpdateScheduler {
     private static final long MINUTES = 60 * SECONDS;
 
     private final AlbumsUpdater albumsUpdater;
+    private final JdbcTemplate jdbcTemplate;
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private JdbcTemplate jdbcTemplate;
-
-
-    public AlbumsUpdateScheduler(DataSource dataSource, AlbumsUpdater albumsUpdater) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater, DataSource dataSource) {
         this.albumsUpdater = albumsUpdater;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 
     @Scheduled(initialDelay = 15 * SECONDS, fixedRate = 2 * MINUTES)
     public void run() {
         try {
-
-            if (startAlbumSchedulerTask()) {
+            if(startAlbumSchedulerTask()){
                 logger.debug("Starting albums update");
                 albumsUpdater.update();
                 logger.debug("Finished albums update");
-
             } else {
-                logger.debug("Nothing to start");
+                logger.debug("No service started");
             }
-
         } catch (Throwable e) {
             logger.error("Error while updating albums", e);
         }
     }
-
 
     private boolean startAlbumSchedulerTask() {
         int updatedRows = jdbcTemplate.update(
